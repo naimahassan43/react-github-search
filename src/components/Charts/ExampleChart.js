87fa0a180c54ee50d9addf667dd8ad0c0240d148
Naima Hassan <naima.hassan434@gmail.com>
2022-06-12 12:10:13 +0100

    Fusion Charts- Pie Chart

--- src/components/Charts/ExampleChart.js
+++ src/components/Charts/ExampleChart.js
@@ -17,13 +17,9 @@ import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
 // Adding the chart and theme as dependency to the core fusioncharts
 ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
 
-// STEP 2 - Chart Data
-
-// STEP 3 - Creating the JSON object to store the chart configurations
-
 const ChartComponent = ({ data }) => {
   const chartConfigs = {
-    type: "column2d", // The chart type
+    type: "pie2d", // The chart type
     width: "400", // Width of the chart
     height: "400", // Height of the chart
     dataFormat: "json", // Data type
