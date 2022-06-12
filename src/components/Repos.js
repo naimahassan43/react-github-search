5eee7527fb636d75f2e0b84c4f27fc7f5b6f49a2
Naima Hassan <naima.hassan434@gmail.com>
2022-06-12 22:41:09 +0100

    Stars and Forks - Functionality

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -23,12 +23,14 @@ const Repos = () => {
     return total;
   }, {});
 
+  //most used language
   const mostUsed = Object.values(languages)
     .sort((a, b) => {
       return b.value - a.value;
     })
     .slice(0, 5);
 
+  //most stars per language
   const mostPopular = Object.values(languages)
     .sort((a, b) => {
       return b.stars - a.stars;
@@ -37,6 +39,22 @@ const Repos = () => {
       return { ...item, value: item.stars };
     })
     .slice(0, 5);
+
+  // stars, forks
+  let { stars, forks } = repos.reduce(
+    (total, item) => {
+      const { stargazers_count, name, forks } = item;
+      total.stars[stargazers_count] = { label: name, value: stargazers_count };
+      total.forks[forks] = { label: name, value: forks };
+
+      return total;
+    },
+    { stars: {}, forks: {} }
+  );
+
+  stars = Object.values(stars).slice(-5).reverse();
+  forks = Object.values(forks).slice(-5).reverse();
+  console.log(stars);
 
   const chartData = [
     {
@@ -57,9 +75,9 @@ const Repos = () => {
     <section className="section">
       <Wrapper className="section-center">
         <Pie3D data={mostUsed} />
-        <Column3D data={chartData} />
+        <Column3D data={stars} />
         <Doughnut2D data={mostPopular} />
-        <Bar3D data={chartData} />
+        <Bar3D data={forks} />
       </Wrapper>
     </section>
   );
